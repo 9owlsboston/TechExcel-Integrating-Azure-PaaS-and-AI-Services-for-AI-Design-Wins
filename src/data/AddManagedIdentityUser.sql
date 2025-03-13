5fda3940fc010384933d8aaa1af4b459dea8d035
9owlsboston <9owlsboston@gmail.com>
2025-03-13 01:59:18 -0700

    day1: ex02-09

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER "id-c2phlbmqs62qg" FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER "id-c2phlbmqs62qg";
+ALTER ROLE db_datawriter ADD MEMBER "id-c2phlbmqs62qg";
 
